0c273b8c24e3a9c76757dde4ac95d704ea41b64a
Vamsikrishna9432 <krishnavamsi9432@gmail.com>
2023-09-21 15:33:18 +0530

    small change

--- src/components/WhatsOnYourMind.jsx
+++ src/components/WhatsOnYourMind.jsx
@@ -8,7 +8,7 @@ const WhatsOnYourMind = ({intrestdata}) => {
   return (
     <div className="relative w-10/12 m-auto  my-1 flex-shrink-0">
         <h1 className="font-bold text-[28px] text-blacks my-3 mx-3">
-           What's on your mind? Choose one
+           What's on your mind?
         </h1>
         <div className="flex overflow-x-scroll scrollbar-hide scroll-smooth" ref={ref}>
           {intrestdata?.map((each) => (
